7bde418d5001e3be4a5429af52f251c531f7fb53
PammerJoe <60983077+PammerJoe@users.noreply.github.com>
2022-12-19 12:12:18 +0100

    Update activity.js

--- mail_activity_team/static/src/models/activity/activity.js
+++ mail_activity_team/static/src/models/activity/activity.js
@@ -33,7 +33,7 @@ registerClassPatchModel('mail.activity', 'mail_activity_team/static/src/models/a
     /*
     * Set assigned team member to current user.
     */
-    async takeActivity() {
+    async _takeActivity() {
         await this.async(() => this.env.services.rpc({
             model: 'mail.activity',
             method: 'set_assigned_team_member',
