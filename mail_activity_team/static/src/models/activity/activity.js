dc529acec93abbe79215697c5958e8eae9974cf8
PammerJoe <60983077+PammerJoe@users.noreply.github.com>
2022-12-19 13:44:04 +0100

    Testing team_member_ids

--- mail_activity_team/static/src/models/activity/activity.js
+++ mail_activity_team/static/src/models/activity/activity.js
@@ -5,7 +5,7 @@ import {
     registerInstancePatchModel,
     registerFieldPatchModel
 } from'@mail/model/model_core';
-import { attr, many2one } from '@mail/model/model_field';
+import { attr, many2one, many2many } from '@mail/model/model_field';
 
 registerFieldPatchModel('mail.activity', 'mail_activity_team/static/src/models/activity.js', {
     /**
@@ -13,6 +13,7 @@ registerFieldPatchModel('mail.activity', 'mail_activity_team/static/src/models/a
      */
     team_name: attr(),
     assigned_team_member: attr(),
+    team_member_ids: attr(),
 });
 
 registerClassPatchModel('mail.activity', 'mail_activity_team/static/src/models/activity.js', {
@@ -25,7 +26,10 @@ registerClassPatchModel('mail.activity', 'mail_activity_team/static/src/models/a
             res.team_name = data.team_name;
         }
         if ('assigned_team_member' in data) {
-            res.assigned_team_member = data.assigned_team_member
+            res.assigned_team_member = data.assigned_team_member;
+        }
+        if ('team_member_ids' in data) {
+            res.team_member_ids = data.team_member_ids;
         }
         return res;
     },
