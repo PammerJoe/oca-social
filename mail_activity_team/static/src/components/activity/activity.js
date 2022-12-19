33329684d356cda771d9604a2db812b3c8b48cd8
PammerJoe <60983077+PammerJoe@users.noreply.github.com>
2022-12-19 12:07:12 +0100

    Update activity.js

--- mail_activity_team/static/src/components/activity/activity.js
+++ mail_activity_team/static/src/components/activity/activity.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 
+import { Activity } from '@mail/components/activity/activity';
 import { patch } from 'web.utils';
 
 patch(Activity.prototype, 'mail_activity_team/static/src/components/activity/activity.js', {
