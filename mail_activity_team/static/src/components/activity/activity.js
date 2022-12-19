9b28c89fe73f815168607391543fb1f646494296
PammerJoe <60983077+PammerJoe@users.noreply.github.com>
2022-12-19 12:18:19 +0100

    Repairing issue with xml template

--- mail_activity_team/static/src/components/activity/activity.js
+++ mail_activity_team/static/src/components/activity/activity.js
@@ -4,11 +4,11 @@
 import { Activity } from '@mail/components/activity/activity';
 import { patch } from 'web.utils';
 
-//patch(Activity.prototype, 'mail_activity_team/static/src/components/activity/activity.js', {
-//    async _onClickTake(ev) {
-//        // do some stuff here
-//        await this.activity.takeActivity();
-//        this.trigger('reload', { keepChanges: true });
-//        return;
-//    }
-//});
+patch(Activity.prototype, 'mail_activity_team/static/src/components/activity/activity.js', {
+    async _onClickTake(ev) {
+        // do some stuff here
+        await this.activity.takeActivity();
+        this.trigger('reload', { keepChanges: true });
+        return;
+    }
+});
